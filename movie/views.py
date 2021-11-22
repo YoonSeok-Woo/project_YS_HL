ec1363a03f035233bfba912c31fd34edb785f781
YoonSeok Woo <wysf1994@naver.com>
2021-11-22 09:07:05 +0900

    1122_indexview

--- movie/views.py
+++ movie/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
-# Create your views here.
+from django.shortcuts import redirect, get_object_or_404
+from django.views.decorators.http import require_safe, require_POST, require_http_methods,require_GET
+from django.contrib.auth.decorators import login_required
+from .models import Movie, Genre, Rates
+
+@require_GET
+def home(request):
+    movies = Movie.order_by('?')[:10]
+    context = {
+        'movies':movies
+    }
+    return render(request, 'movies/home.html', context)
