0080623c0a95ef2aa5afc3107dc71158039fdbe3
unknown <honglim99@naver.com>
2021-11-25 03:06:03 +0900

    1125 communty/detail.html

--- community/forms.py
+++ community/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.forms.widgets import Textarea
 from . models import Community,Comment
 
 class CommunityForm(forms.ModelForm):
@@ -7,6 +8,16 @@ class CommunityForm(forms.ModelForm):
         fields = ('title','content','image')
 
 class CommentForm(forms.ModelForm):
+    content = forms.CharField(
+        label = "새 댓글",
+        widget = forms.Textarea(
+            attrs={
+                'class': 'col-auto',
+                'placeholder': '댓글을 입력해주세요',
+                'rows': '1',
+            }
+        )
+    )
     class Meta:
         model = Comment
         exclude = ('community','user')
